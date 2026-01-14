5f10f0d3af6a08ed79ff9909d62806f8ff0f4ba4
David W <davidww86@hotmail.com>
2026-01-13 20:28:54 -0500

    interviewbit_MATH_primeNumbers

---
+++ Interviewbit/Math/Python/primeNum/primeNum.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 13 20:15:39 2026
+
+@author: USER
+"""
+
+"""
+Problem Description
+Given a number A, find all prime numbers up to A (A included).
+Make sure the returned array is sorted.
+
+Problem Constraints
+1 <= A <= 106
+Input Format
+The first argument is an integer A.
+
+Output Format
+Return array of integers.
+Example Input
+A = 7
+Example Output
+[2, 3, 5, 7]
+
+Example Explanation
+All primes till 7 are, 2, 3, 5 and 7
+class Solution:
+	# @param A : integer
+	# @return a list of integers
+	def sieve(self, A):
+"""
+def sieve(A):
+    pass
+    
+    
---
+++ Interviewbit/Math/Python/primeNum/test_primeNum.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 13 20:17:20 2026
+
+@author: USER
+"""
+
+import primeNum
+import pytest
+
+def test_sieve(A):
+    # assert primeNum.sieve(7) == [2,3,5,7]
+    pass
